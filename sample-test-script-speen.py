1eadf63d0c0702bb6209b84e050f3d3afb4ffc4e
Vasilis Georgitzikis <tzikis@gmail.com>
2019-03-28 13:40:31 +0200

    new current measurements

--- sample-test-script-speen.py
+++ sample-test-script-speen.py
@@ -16,17 +16,20 @@ def test_measure_power_consumption():
     # time.sleep(5)
 
     INA219 = SpannerTestboard.INA219
-    time.sleep(1)
 
     print("Measuring Shunt Voltage & Current")
     print("Shunt Voltage (mV):")
-    voltage = testboard.ina219_getValue(INA219.SHUNT_VOLTAGE_MV)
-    print(voltage)
-    time.sleep(1)
-
+    shunt_voltage_mv = testboard.ina219_getValue(INA219.SHUNT_VOLTAGE_MV)
+    print(shunt_voltage_mv)
 
     print("Current consumption (mA):")
-    current = testboard.ina219_getValue(INA219.CURRENT_MA)
+    shunt_resistor = 0.02
+    current = (shunt_voltage_mv * 1000) /  shunt_resistor
+    print(current)
+
+    print("Current consumption (mA):")
+    current_multiplier = 5
+    current = testboard.ina219_getValue(INA219.CURRENT_MA) * current_multiplier
     print(current)
     time.sleep(1)
 
